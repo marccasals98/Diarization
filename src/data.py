8562ae466f4211c9e5a9d1a86a1f39a039cacec0
marccasals98 <marc.casals8@gmail.com>
2025-02-24 18:14:31 +0100

    The code is running without any problem. I need to check that, for each index, I am getting the results that I expect.
    Then I'll need to do:
    2. Document all the methods and functions. We need to implement docstring and make the code clean.
    
    3. Remove unecessary functions. There is still some code from when we were using Fishers dataset. We should remove it.

--- src/data.py
+++ src/data.py
@@ -6,49 +6,100 @@ from io import StringIO
 import torchaudio
 import numpy as np
 import torch
+from tqdm import tqdm
 
 def load_audio(audio_path)->list:
     audio, sr = torchaudio.load(audio_path)
     return audio, sr
 
-def read_rttm(rttm_path)->list:
+def read_rttm(rttm_path)->Tuple[list, list]:
+    results = []
+    speakers = []
     with open(rttm_path, 'r') as f:
-        lines = f.readlines()
-    list_lines = []
-    for line in lines:
-        list_lines.append(line.split())
-    return list_lines
+        for line in f:
+            # Skip comment lines and any blank lines
+            if line.startswith('#') or not line.strip():
+                continue
+            parts = line.strip().split()
+            # Make sure the line has at least 8 elements
+            if len(parts) >= 8:
+                # Extract the 4th, 5th, and 8th elements.
+                # (Indexes 3, 4, and 7 in Python's 0-indexed lists)
+                start = float(parts[3])
+                duration = float(parts[4])
+                speaker = parts[7]
+                results.append((start, duration, speaker))
+                speakers.append(speaker)
+    return results, speakers
+
+def get_files_of_path(path: str)->list:
+    files = []
+    for root, _, filenames in os.walk(path):
+        for filename in filenames:
+            files.append(os.path.join(root, filename))
+    return files
+
+def speaker_to_index(speaker_list: list)->dict:
+    """Maps speaker names to indices.
+
+    {"speakerA": 0, "speakerB": 1, "speakerC": 2}
+
+    Args:
+        speaker_list (list): the list of speaker names
+
+    Returns:
+        dict: mapping of speaker names to indices
+    """
+    speaker_indices = {}
+    for i, speaker in enumerate(speaker_list):
+        speaker_indices[speaker] = i
+    return speaker_indices
 
 class TrainDataset(data.Dataset):
     def __init__(self, 
-                audio_path,
-                transcriptions_path, 
+                audio_files,
+                rttm_paths, 
                 segment_length, 
                 allow_overlap, 
-                num_speakers,
+                max_num_speakers,
+                transform=None,
                 frame_length=100):
         
-        self.audio_path = audio_path
-        self.transcriptions_path = transcriptions_path
+        self.audio_files = audio_files
+        self.rttm_paths = rttm_paths
         self.segment_length = segment_length
         self.allow_overlap = allow_overlap
-        self.num_speakers = num_speakers
+        self.max_num_speakers = max_num_speakers
+        self.transform = transform
         self.frame_length = frame_length
-        self.open_files()
+        self.precompute_segments()
 
     def precompute_segments(self):
+        """This function reads the audio files and RTTM files from the paths
+        provided in the constructor and precomputes the segments for the
+        dataset.
+        """
+        print("Precomputing segments...")
         self.segments = []
-        for audio_file, rttm_file in zip(self.audio_files, self.rttm_paths):
-            turns = read_rttm(rttm_file)
+        audio_files = get_files_of_path(self.audio_files)
+        rttm_files = get_files_of_path(self.rttm_paths)
+
+        if len(audio_files) != len(rttm_files):
+            print(f"audio_files: {len(audio_files)}")
+            print(f"rttm_files: {len(rttm_files)}")
+            raise ValueError("The number of audio files and RTTM files must match.")
+
+        for audio_file, rttm_file in tqdm(zip(audio_files, rttm_files), total=len(audio_files)):
+            turns, speakers = read_rttm(rttm_file)
             audio, sr = load_audio(audio_file)
             n_samples =len(audio[0])
             seg_samples = int(self.segment_length * sr)
 
             for start in range(0, n_samples-seg_samples + 1, seg_samples):
-                labels = self.compute_labels_for_segment(turns, start / sr, (start + seg_samples) / sr)
+                labels = self.compute_labels_for_segment(turns, start / sr, (start + seg_samples) / sr, speakers)
                 self.segments.append((audio_file, start / sr, labels))
 
-    def compute_labels_for_segment(self, turns: list, start: float, end: float)->np.array:
+    def compute_labels_for_segment(self, turns: list, start: float, end: float, speakers: list)->np.array:
         """This function converts the speaker turn information from your RTTM
         file into a frame-level label vector for a fixed-length audio segment.
 
@@ -60,27 +111,39 @@ class TrainDataset(data.Dataset):
         Returns:
             np.array: _description_
         """
-        # HACK put frames per second as hyperparemeter.
-        n_frames = int(self.segment_length * self.frame_length) # self.frame_length frames per second
+
+        # calculate the number of frames in the segment
+        n_frames = int(self.segment_length * self.frame_length)
+
+        # Create a mapping of speaker names to indices
+        num_speakers = len(speakers)
+        speaker_indices = speaker_to_index(speakers)
+
+        # Initialize the label vector
         if self.allow_overlap == True:
-            label_vector = np.zeros((n_frames, self.num_speakers), dtype=int)
+            label_vector = np.zeros((n_frames, num_speakers), dtype=int)
         else:
             label_vector = np.zeros(n_frames, dtype=int)
-        
+
+        # Iterate over the speaker turns
         for start_time, duration, speaker in turns:
             end_time = start_time + duration
             # We need to check if the speaker turn is within the segment
             if end_time > start and start_time < end:
+
+                # Convert the start and end times to frame indices
                 start_frame = max(0, int((start_time - start) * self.frame_length))
                 end_frame = min(n_frames, int((end_time - start) * self.frame_length))
 
+                # Set the label vector values for the speaker turn
                 if self.allow_overlap == True:
-                    label_vector[start_frame:end_frame, speaker] = 1
+                    label_vector[start_frame:end_frame, speaker_indices[speaker]] = 1
                 else:
                     label_vector[start_frame:end_frame] = speaker
 
         return label_vector
     
+    #region remove?
     def read_tdf(self, path) -> Tuple[dict, pd.DataFrame]:
         with open(path, 'r', encoding='utf-8') as f:
             lines = f.readlines()
@@ -118,19 +181,20 @@ class TrainDataset(data.Dataset):
         return metadata, data        
 
     def open_files(self):
-        for root, paths, files in os.walk(self.transcriptions_path):
+        for root, paths, files in os.walk(self.rttm_paths):
             for file in files:
                 print(f"path: {os.path.join(root,file)}")
                 metadata, data = self.read_tdf(os.path.join(root,file))
             print(f"metadata: {metadata}")
             print(f"data: {data}")
-    
+    #endregion
+
     def __len__(self):
         return len(self.segments)
     
     def __getitem__(self, index):
         audio_file, start_time, labels = self.segments[index]
-        audio, sr = load_audio(audio_file, self.sr)
+        audio, sr = load_audio(audio_file)
         start_sample = int(start_time * sr)
         seg_samples = int(self.segment_length * sr)
         audio_segment = audio[:, start_sample:start_sample + seg_samples]
@@ -146,3 +210,16 @@ class TrainDataset(data.Dataset):
         audio_tensor = torch.tensor(audio_segment, dtype=torch.float)
         label_tensor = torch.tensor(labels, dtype=torch.long)
         return audio_tensor, label_tensor
+
+data = TrainDataset(
+    audio_files="/gpfs/projects/bsc88/speech/data/raw_data/diarization/voxconverse/audio/dev/audio",
+    rttm_paths="/gpfs/projects/bsc88/speech/data/raw_data/diarization/voxconverse/dev",
+    segment_length=5,
+    allow_overlap=True,
+    max_num_speakers=2,
+    frame_length=100
+)
+audio_label = data[0]
+audio_tensor, label_tensor = audio_label
+print(audio_tensor.shape)
+print(label_tensor.shape)
